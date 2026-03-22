204a7a8d6e8047149ef06eb8c6c996fe7ad890c9
akshaya-sgr <akshaysgr@gmail.com>
2026-03-22 21:10:28 +0530

    from qa branch

--- DButils.java
+++ DButils.java
@@ -1,4 +1,5 @@
 hi this file cnsists of DB regaurding files
 created in master branch 
 this line is created in dev branch
-now we are learning branching , this line is from master 
+now we are learning branching , this line is from master
+this is a line of code created in nw qa branch 
